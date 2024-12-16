adf6b3a18097c16a895baa735bd976b1c71cc6bd
Daiana Barbosa Soares <daiana.soares@b2wdigital.com>
2024-12-16 20:39:40 -0300

    adding SqsMessageService class and SqsController class that adds message to SQS queue with /sqs/sendMessage endpoint

--- src/main/java/com/example/rest/service/SqsMessageService.java
+++ src/main/java/com/example/rest/service/SqsMessageService.java
@@ -1,32 +1,32 @@
 package com.example.rest.service;
 
-//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-//import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
-//import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Service
 public class SqsMessageService {
-//
-//    @Value("queue-java-test")
-//    private String queueName;
-//
-//    private final SqsAsyncClient sqsAsyncClient;
-//
-//    public SqsMessageService(SqsAsyncClient sqsAsyncClient) {
-//        this.sqsAsyncClient = sqsAsyncClient;
-//    }
-//
-//
-//    public  void sendMessage(String message) {
-//        SendMessageRequest request = SendMessageRequest.builder()
-//                .queueUrl("http://localhost:4566/000000000000/queue-java-test")
-//                .messageBody(message)
-//                .build();
-//
-//        sqsAsyncClient.sendMessage(request);
-//        System.out.println(" Mensagem enviada");
-//
-//    }
+
+    @Value("queue-java-test")
+    private String queueName;
+
+    private final SqsAsyncClient sqsAsyncClient;
+
+    public SqsMessageService(SqsAsyncClient sqsAsyncClient) {
+        this.sqsAsyncClient = sqsAsyncClient;
+    }
+
+
+    public  void sendMessage(String message) {
+        SendMessageRequest request = SendMessageRequest.builder()
+                .queueUrl("http://localhost:4566/000000000000/queue-java-test")
+                .messageBody(message)
+                .build();
+
+        sqsAsyncClient.sendMessage(request);
+        System.out.println(" Mensagem enviada");
+
+    }
 
 }
